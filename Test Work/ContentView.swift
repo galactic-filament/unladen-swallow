e658b6ae7be02ff8b6c4dc3c2161d182a16c07f9
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2022-04-10 11:55:32 -0700

    Initial Commit

---
+++ Test Work/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Work
+//
+//  Created by Adrian Parker on 4/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test Work/Test_WorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_WorkApp.swift
+//  Test Work
+//
+//  Created by Adrian Parker on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Test_WorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
